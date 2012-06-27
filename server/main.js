15f79ee1bddaad60f6b9780405b125e0902047e2
flufy3d <flufy3d@gmail.com>
2012-06-27 16:31:02 +0800

    deploy new version to jitsu add configuration variable

--- server/main.js
+++ server/main.js
@@ -1,14 +1,22 @@
+////Config Variable
+//var sil_port = 8000;
+//var database_url = 'mongodb://127.0.0.1/deldb';
+var sil_port = 80;
+var database_url = 'mongodb://nodejitsu:c0e78e5b3ab649484028fb71a44ff623@staff.mongohq.com:10046/nodejitsudb831935829387';
+
+///////////////////////////////////////////////////////
 //// init database components
 var mongoose = require('mongoose'); 
 
 console.log('Started database connection, waiting for it to open');
+console.log(database_url);
 
 var db_connect_timeout = setTimeout(function() {
   console.log('Failed to connect database!');
   //throw 'Failed to connect database!'
 }, 10000);
 
-mongoose.connect('mongodb://127.0.0.1/deldb');
+mongoose.connect(database_url);
 console.log('database ip and port:'+mongoose.connection.host+':'+mongoose.connection.port);
 
 
@@ -34,7 +42,7 @@ var User = mongoose.model('User', User);
 
 
 
-var io = require('socket.io').listen(8000);
+var io = require('socket.io').listen(sil_port);
 
 io.sockets.on('connection', function (socket) {
    var address = socket.handshake.address;
