9a4866d955cafabb14ee498de67630c7b694a256
flufy3d <flufy3d@gmail.com>
2012-06-27 15:46:44 +0800

    Complete the registration and login modules

--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
+//// init database components
 var mongoose = require('mongoose'); 
+
 console.log('Started database connection, waiting for it to open');
 
 var db_connect_timeout = setTimeout(function() {
@@ -6,86 +8,67 @@ var db_connect_timeout = setTimeout(function() {
   //throw 'Failed to connect database!'
 }, 10000);
 
-mongoose.connect('mongodb://127.0.0.1/my_database');
-console.log(mongoose.connection.host);
-console.log(mongoose.connection.port);
+mongoose.connect('mongodb://127.0.0.1/deldb');
+console.log('database ip and port:'+mongoose.connection.host+':'+mongoose.connection.port);
 
- mongoose.connection.on('open', function() {
-                console.log('Opened connection');
+
+mongoose.connection.on('open', function() {
+                console.log('Opened database connection');
                 clearTimeout(db_connect_timeout); 
             });
             
 var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
-var Comments = new Schema({
-    title     : String
-  , body      : String
-  , date      : Date
+var User = new Schema({
+    name      : String
+  , email     : String
+  , password  : String
+  , date      : { type: Date, default: Date.now }
 });
 
-var BlogPost = new Schema({
-    author    : ObjectId
-  , title     : String
-  , body      : String
-  , date      : Date
-  , comments  : [Comments]
-  , meta      : {
-        votes : Number
-      , favs  : Number
-    }
-  , count     : Number
-});
 
-var BlogPost = mongoose.model('BlogPost', BlogPost);
+var User = mongoose.model('User', User);
 
-/* push new one
-var post = new BlogPost();
-post.title='asdf';
-post.count= 23;
-// create a comment
-post.comments.push({ title: 'My comment' });
 
-post.save(function (err) {
-  if(err){
-      throw err;
-      console.log(err);
-  }else{
-      console.log('saved!');
-  }
-});
-*/
-/* update one
-BlogPost.findOne({ title: 'asdf' }, function (err, doc){
-  doc.title = 'jason borne';
-  doc.count += 1;
-  doc.save();
-});
-*/
+/////////////////////////////////////////////////////////
 
-/* delete one
-BlogPost.findOne({ count: 23 }, function (err, doc){
-  doc.remove();
-});
-*/
 
-/* Traverse all the results
-BlogPost.find({}, function (err, docs) {
-  // docs.forEach
-  docs.forEach(function(doc){console.log(doc)});
-});
-*/
-///////////
+
 var io = require('socket.io').listen(8000);
 
 io.sockets.on('connection', function (socket) {
-  console.log('connected');
+   var address = socket.handshake.address;
+   console.log("New connection from " + address.address + ":" + address.port);
+   
   socket.on('register',function(data) {
     console.log(data);
-    socket.emit('register_response',{result:'OK'});
-  
+    var user = new User();
+    user.name       = data.name;
+    user.email      = data.email;
+    user.password   = data.password;
+    
+    user.save(function (err) {
+        if(err){
+            console.log(err);
+            socket.emit('register_response',{result:err});
+        }else{
+            console.log('new user registered!');
+            socket.emit('register_response',{result:'OK'});
+        }
+    });
+      
   });
+  
   socket.on('login', function (data) {
-    console.log(data);
-    socket.emit('login_response',{result:'OK'});
+    console.log('user login request.');
+    User.findOne({ email: data.email,password: data.password }, function (err, doc){
+        if (doc != null){
+            socket.emit('login_response',{result:'OK'});
+        }
+        else{
+            socket.emit('login_response',{result:err});
+        }
+    });
+    
   });
 });
